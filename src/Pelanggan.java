4e9ea9f0fa57c8edb719857add162696e6ef64d7
ahmadriskikurniawan86-rgb <ahmadriskikurniawan86@gmail.com>
2026-01-02 23:40:36 +0800

    Penyesuaian AntrianManager dan Pelanggan (Business Logic)
    Menyesuaikan AntrianManager menggunakan objek Pelanggan,
    FIFO queue, pencatatan riwayat layanan, dan perbaikan main
    untuk pengujian statistik antrian.

--- src/Pelanggan.java
+++ src/Pelanggan.java
@@ -1,2 +1,25 @@
 public class Pelanggan {
+
+    private String nama;
+    private String nomorAntrian;
+
+    // Constructor
+    public Pelanggan(String nama) {
+        this.nama = nama;
+    }
+
+    // Getter nama
+    public String getNama() {
+        return nama;
+    }
+
+    // Setter nomor antrian
+    public void setNomorAntrian(String nomorAntrian) {
+        this.nomorAntrian = nomorAntrian;
+    }
+
+    // Getter nomor antrian
+    public String getNomorAntrian() {
+        return nomorAntrian;
+    }
 }
--- src/main.java
+++ src/main.java
@@ -9,11 +9,17 @@ public class main {
         System.out.println(manager.tambahPelanggan("Andi"));
 
         System.out.println("\nLayani pelanggan:");
-        System.out.println(manager.ambilPelangganBerikutnya());
-        System.out.println(manager.ambilPelangganBerikutnya());
+        Pelanggan p1 = manager.ambilPelangganBerikutnya();
+        manager.tambahKeRiwayat(p1);
+        System.out.println(p1.getNomorAntrian() + " - " + p1.getNama());
+
+        Pelanggan p2 = manager.ambilPelangganBerikutnya();
+        manager.tambahKeRiwayat(p2);
+        System.out.println(p2.getNomorAntrian() + " - " + p2.getNama());
+
 
         System.out.println("\nStatistik:");
         System.out.println("Sisa antrian: " + manager.getJumlahAntrian());
-        System.out.println("Total dilayani: " + manager.getTotalDilayani());
+        System.out.println("Total dilayani: " + manager.getRiwayatLayanan().size());
     }
 }
